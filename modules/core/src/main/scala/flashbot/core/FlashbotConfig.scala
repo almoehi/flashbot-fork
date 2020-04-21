c720cb2be2a29338e292896897587e5f61c20002
hrapp <almoehi@gmail.com>
2020-04-21 22:10:39 +0200

    fixed: retentionFor throwing exception when no DataPath is available

--- modules/core/src/main/scala/flashbot/core/FlashbotConfig.scala
+++ modules/core/src/main/scala/flashbot/core/FlashbotConfig.scala
@@ -45,9 +45,10 @@ object FlashbotConfig {
     def filterSources(sources: Set[String]): Set[String] =
       filterIngestSources(sources) ++ filterBackfillSources(sources)
 
-    def retentionFor(path: DataPath[_]): Option[FiniteDuration] =
-      retention.find(record => DataPath(record.head).matches(path))
+    def retentionFor(path: DataPath[_]): Option[FiniteDuration] = {
+      retention.filter(_.nonEmpty).find(record => DataPath(record.head).matches(path))
         .map(record => time.parseDuration(record.last))
+    }
   }
 
   @ConfiguredJsonCodec(decodeOnly = true)
