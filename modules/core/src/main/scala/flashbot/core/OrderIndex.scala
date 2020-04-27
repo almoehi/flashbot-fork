08914d052cffb017a38eae88e8f91a991e5a85ce
hrapp <almoehi@gmail.com>
2020-04-27 15:35:10 +0200

    added: implementations of add() and remove() methods

--- modules/core/src/main/scala/flashbot/core/OrderIndex.scala
+++ modules/core/src/main/scala/flashbot/core/OrderIndex.scala
@@ -1,17 +1,30 @@
 package flashbot.core
 
+import scala.util.Try
+
 class OrderIndex {
   val byTag = new java.util.HashMap[String, java.util.HashMap[String, OrderRef]]
   val byKey = new java.util.HashMap[String, OrderRef]
-  val byClientId = new java.util.HashMap[String, OrderRef]
+  val byClientId = new java.util.HashMap[String, OrderRef]  // TODO: check where clientId comes from ?
   var counter: Long = 0
 
   protected[flashbot] def insert(order: OrderRef): Unit = {
     order.seqNr = counter
     counter += 1
+    byKey.put(order.key, order)
+    byTag.computeIfAbsent(order.tag, (key: String) => {
+      new java.util.HashMap[String,OrderRef](){
+        put(key, order)
+      }
+    })
+    // TODO: whre to get clientId ?
+    //byClientId.put(order)
   }
 
   protected[flashbot] def remove(order: OrderRef): Unit = {
+    Try(byKey.remove(order.key))
+    Try(byTag.remove(order.tag))
+
   }
 }
 
