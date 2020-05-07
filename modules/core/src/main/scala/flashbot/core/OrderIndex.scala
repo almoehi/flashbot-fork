a4608b241e25dad07149c817cc62b0a4320dcd91
hrapp <almoehi@gmail.com>
2020-05-07 17:11:46 +0200

    fixed: store orders by id

--- modules/core/src/main/scala/flashbot/core/OrderIndex.scala
+++ modules/core/src/main/scala/flashbot/core/OrderIndex.scala
@@ -5,26 +5,25 @@ import scala.util.Try
 class OrderIndex {
   val byTag = new java.util.HashMap[String, java.util.HashMap[String, OrderRef]]
   val byKey = new java.util.HashMap[String, OrderRef]
-  val byClientId = new java.util.HashMap[String, OrderRef]  // TODO: check where clientId comes from ?
+  val byClientId = new java.util.HashMap[String, OrderRef]
   var counter: Long = 0
 
   protected[flashbot] def insert(order: OrderRef): Unit = {
     order.seqNr = counter
     counter += 1
     byKey.put(order.key, order)
+    byClientId.put(order.id, order)
     byTag.computeIfAbsent(order.tag, (key: String) => {
       new java.util.HashMap[String,OrderRef](){
         put(key, order)
       }
     })
-    // TODO: whre to get clientId ?
-    //byClientId.put(order)
   }
 
   protected[flashbot] def remove(order: OrderRef): Unit = {
     Try(byKey.remove(order.key))
     Try(byTag.remove(order.tag))
-
+    Try(byClientId.remove(order.id))
   }
 }
 
