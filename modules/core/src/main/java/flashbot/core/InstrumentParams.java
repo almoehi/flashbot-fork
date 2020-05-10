20f45810fd5966207b15c31543cf222ab7fc6c5c
hrapp <almoehi@gmail.com>
2020-05-10 12:28:11 +0200

    added: minOrderSize exchange param

--- modules/core/src/main/java/flashbot/core/InstrumentParams.java
+++ modules/core/src/main/java/flashbot/core/InstrumentParams.java
@@ -6,6 +6,8 @@ public class InstrumentParams {
 
     // The minimum amount that the price can move by.
     public double tickSize = Double.NaN;
+    // Min. order size
+    public double minOrderSize = Double.NaN;
 
     /**
       * This constructor must be empty, to serve as an implementation of "default" params.
@@ -13,10 +15,11 @@ public class InstrumentParams {
     public InstrumentParams() {
     }
 
-    public InstrumentParams(double makerFee, double takerFee, double tickSize) {
+    public InstrumentParams(double makerFee, double takerFee, double tickSize, double minOrderSize) {
         this.makerFee = makerFee;
         this.takerFee = takerFee;
         this.tickSize = tickSize;
+        this.minOrderSize =  minOrderSize;
     }
 
     public static final InstrumentParams DEFAULT = new InstrumentParams();
